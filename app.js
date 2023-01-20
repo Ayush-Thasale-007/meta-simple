4cc1d5ecce0efdd67450c2114d75fa3787188e83
Apricot <83486785+Ayush-Thasale-007@users.noreply.github.com>
2023-01-20 20:36:37 +0530

    simple try

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-// upload();
-const globalErrorHandler = require("./controllers/errorCotroller");
 
 const app = express();
 app.use(cors());
@@ -16,7 +14,6 @@ app.use("/ping", (req, res, next) => {
   res.send("Helo World !");
 });
 
-app.use(globalErrorHandler);
 
 module.exports = app;
 // http://localhost:3000/api/v1/blogs/62cfcd969c828fea45257607/upload
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,15 +4,6 @@ const authController = require("./../controllers/authController");
 
 const router = express.Router();
 
-router.post("/signup", authController.signup);
-router.post("/login", authController.login);
-
-router.patch(
-  "/updateMyPassword",
-  authController.protect,
-  authController.updatePassword
-);
-
-router.get("/me", authController.protect, authController.getUser);
+router.post("/signup", authController.createUser);
 
 module.exports = router;
